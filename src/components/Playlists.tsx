4cf6417c28dd3524cccce996b58db7379424ba1b
Matt <mqunell@pm.me>
2023-07-03 18:56:08 -0700

    remove unnecessary fragment

--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -45,53 +45,51 @@ const Playlists = ({ playlists }: Props): JSX.Element => {
 	if (!playlists.length) return <p>Loading...</p>;
 
 	return (
-		<>
-			<section className="mx-auto flex w-full max-w-xl flex-col gap-4 lg:max-w-7xl">
-				<div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
-					<div>
-						<input
-							className="mr-auto w-64 rounded border border-slate-300 px-3 py-1 text-black"
-							type="text"
-							placeholder="Filter by song, artist, or album"
-							onChange={(e) => setFilter(e.target.value)}
-						/>
-						<p>
-							{displayPlaylists.length !== playlists.length &&
-								displayPlaylists.length + '/'}
-							{playlists.length} Playlists
-						</p>
-					</div>
-
-					{playlistDurations && (
-						<div>
-							<p>
-								Shortest: {playlistDurations.shortest.name} (
-								{formatTime(playlistDurations.shortest.duration)})
-							</p>
-							<p>
-								Longest: {playlistDurations.longest.name} (
-								{formatTime(playlistDurations.longest.duration)})
-							</p>
-						</div>
-					)}
-					{mostCommonArtists.length && mostCommonArtists[0].count > 1 && (
-						<p className="max-w-xl">
-							Most common artist{mostCommonArtists.length ? 's' : ''}:{' '}
-							{mostCommonArtists.length
-								? mostCommonArtists
-										.map((mca) => mca.artist)
-										.sort()
-										.join(', ')
-								: mostCommonArtists[0].artist}{' '}
-							({mostCommonArtists[0].count})
-						</p>
-					)}
+		<section className="mx-auto flex w-full max-w-xl flex-col gap-4 lg:max-w-7xl">
+			<div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
+				<div>
+					<input
+						className="mr-auto w-64 rounded border border-slate-300 px-3 py-1 text-black"
+						type="text"
+						placeholder="Filter by song, artist, or album"
+						onChange={(e) => setFilter(e.target.value)}
+					/>
+					<p>
+						{displayPlaylists.length !== playlists.length &&
+							displayPlaylists.length + '/'}
+						{playlists.length} Playlists
+					</p>
 				</div>
 
-				<PlaylistsMobile displayPlaylists={displayPlaylists} />
-				<PlaylistsDesktop displayPlaylists={displayPlaylists} filter={filter} />
-			</section>
-		</>
+				{playlistDurations && (
+					<div>
+						<p>
+							Shortest: {playlistDurations.shortest.name} (
+							{formatTime(playlistDurations.shortest.duration)})
+						</p>
+						<p>
+							Longest: {playlistDurations.longest.name} (
+							{formatTime(playlistDurations.longest.duration)})
+						</p>
+					</div>
+				)}
+				{mostCommonArtists.length && mostCommonArtists[0].count > 1 && (
+					<p className="max-w-xl">
+						Most common artist{mostCommonArtists.length ? 's' : ''}:{' '}
+						{mostCommonArtists.length
+							? mostCommonArtists
+									.map((mca) => mca.artist)
+									.sort()
+									.join(', ')
+							: mostCommonArtists[0].artist}{' '}
+						({mostCommonArtists[0].count})
+					</p>
+				)}
+			</div>
+
+			<PlaylistsMobile displayPlaylists={displayPlaylists} />
+			<PlaylistsDesktop displayPlaylists={displayPlaylists} filter={filter} />
+		</section>
 	);
 };
 
